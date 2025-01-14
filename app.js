29bda699da9a00b879c50250500126c78754ad35
Dominik Filyó <40269502+flo0id@users.noreply.github.com>
2025-01-14 16:22:43 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -45,7 +45,6 @@ const connection = mysql.createConnection({
   database: process.env.DB_NAME,
 });
 
-console.log("Connected to MySQL", process.env.DB_PASSWORD, process.env.DB_NAME);
 // Login endpoint
 app.post("/login", (req, res) => {
   const { username, password } = req.body;
